0ad06ede43c2c958bb8617d004b3577d7b777dee
Thishan Herath <diniduthishan9@gmail.com>
2026-02-25 07:17:24 +0530

    home.jsx file and login.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 import { useState } from 'react'
 import './App.css'
 import Products from "./components/Products"
-import{ Route, Routes } from "react-router-dom"
+import{ BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from './pages/Home'
+import Login from './pages/Login'
 
 
 function App() {
   return (
-    <>
-    <routes>
-    <Products />
-    {/* <Route path="/" element={<Products />} /> */}
-      
-    </routes>
-    </>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+    </Routes>
   )
 }
 
 export default App
+
+
+
+{/* <routes> */}
+    // <Products />
+    {/* <Route path="/" element={<Products />} /> */}
+      
+// </routes>
