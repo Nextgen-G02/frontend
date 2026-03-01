de63cf577244f6de73e6759624f4d975c4a671f0
Krishani-imalka <krishani08imalka@gmail.com>
2026-03-02 00:17:45 +0530

    Create ProductTitle Component file and index.jsx file and product.jsx page file

--- src/components/Products/ProductTitle.jsx
+++ src/components/Products/ProductTitle.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Products() {
+function ProductTitle() {
   return (
     <div>
       <h1 className="text-center text-[40px] font-bold text-gray-800 p-[20px]">All Products</h1>
@@ -8,4 +8,4 @@ function Products() {
   );
 }
 
-export default Products;
+export default ProductTitle;
