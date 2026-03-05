d1a211898e85bd7f3a7607c2a7c865cef5fdf66c
Krishani-imalka <krishani08imalka@gmail.com>
2026-03-05 23:39:00 +0530

    CreateUpdate the SearchProduct file and Create ItemProduct.jsx file ,product.jsx file updated

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import SearchProduct from '../components/Products/SearchProduct';
+import ItemProduct from '../components/Products/ItemProduct';
 
 function Products() {
   return (
     <div>
       <SearchProduct />
+      <ItemProduct/>
     </div>
   )
 }
