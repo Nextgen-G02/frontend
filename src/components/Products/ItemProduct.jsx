0044196da9d5b0a963c4dffe51288e876ac58f90
Krishani-imalka <krishani08imalka@gmail.com>
2026-03-06 20:46:18 +0530

    update itemProduct.jsx file

--- src/components/Products/ItemProduct.jsx
+++ src/components/Products/ItemProduct.jsx
@@ -2,16 +2,21 @@ import React from "react";
 
 const ItemProduct = () => {
   return (
-    <div className="w-[260px] rounded-xl shadow-md overflow-hidden bg-secondary font-sans hover:shadow-lg transition ">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 px-6 mt-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 px-6 mt-6">
+
+      <div className="w-full max-w-[260px] rounded-2xl shadow-md overflow-hidden bg-secondary font-sans hover:shadow-lg transition">
+        
         <img
-          src="https://images.unsplash.com/photo-1621303837174-89787a7d4729" alt="product" className="w-full h-[180px] object-cover" />
+          src="https://images.unsplash.com/photo-1621303837174-89787a7d4729"
+          alt="product"
+          className="w-full h-[180px] object-cover"
+        />
 
         <div className="p-4">
 
           <div className="flex justify-between items-center">
-            <h3 className="text-[16px] font-semibold">
-              stawberry cake
+            <h3 className="text-[18px] font-semibold">
+              Strawberry Cake
             </h3>
 
             <span className="text-xs px-2 py-1 rounded bg-primary text-gray-700">
@@ -20,13 +25,12 @@ const ItemProduct = () => {
           </div>
 
           <p className="text-sm text-gray-500 mt-2">
-            High-quality wireless headphones with noise cancellation and long
-            battery life.
+            Delicious strawberry bento cake perfect for small celebrations.
           </p>
 
           <div className="flex justify-between items-center mt-3 mb-3">
-            <span className="text-accent font-bold text-lg">
-              RS.1200.00
+            <span className="text-black font-bold text-lg">
+              Rs.1200.00
             </span>
 
             <span className="text-xs text-gray-500">
@@ -46,6 +50,7 @@ const ItemProduct = () => {
 
         </div>
       </div>
+
     </div>
   );
 };
