bc86b5686992ebf717a8942ab1c7447300d3e0b2
Krishani-imalka <krishani08imalka@gmail.com>
2026-03-14 23:39:34 +0530

    create the admin veiw productDashboard

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import{ BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Products from './pages/Products'
+import AdminProduct from './pages/AdminProduct'
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Route path='/' element={<Home />} />
       <Route path='/login' element={<Login />} />
       <Route path="/products" element={<Products />} />
+      <Route path='/adminproduct' element={<AdminProduct />} />
     </Routes>
   )
 }
